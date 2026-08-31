02e5d18ffae8241d5ecec462b58c88dd0dfbbbf6
Seto <sultanagungper@gmail.com>
2026-08-31 11:36:47 +0700

    fix: compile css and force https for vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,25 +3,19 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // <-- Tambahkan ini
+use Illuminate\Support\Facades\URL; // Tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        // Memaksa seluruh aset menggunakan HTTPS di Vercel
-        if (config('app.env') === 'production') {
+        // Tambahkan 3 baris ini untuk memaksa HTTPS di Vercel
+        if (env('APP_ENV') !== 'local') {
             URL::forceScheme('https');
         }
     }
